d42bf80c43f78f0daa9849fb0d52c8c56d5769ae
mukhammedaliametov <ametovmukhammedali07@gmail.com>
2025-09-27 20:38:50 +0500

    compleated app

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,16 +1,78 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaRegCirclePause, FaRegCirclePlay } from "react-icons/fa6";
 
 const Clock = () => {
   const [pause, setPause] = useState();
+  const [date, setDate] = useState(new Date());
+
   const handlePause = () => setPause(!pause);
 
-  const date = new Date();
+  useEffect(() => {
+    pause
+      ? ""
+      : setTimeout(() => {
+          setDate(new Date());
+        }, 1000);
+  });
+
+  function padZero(num) {
+    return (num < 10 ? "0" : "") + num;
+  }
+
+  let getMonth = date.getMonth() + 1;
+  let monthName = '';
+
+  switch(getMonth){
+    case 1:
+        monthName = 'Jan'
+        break;
+    case 2:
+        monthName = 'Feb'
+        break;
+    case 3:
+        monthName = 'Mar'
+        break;
+    case 4:
+        monthName = 'Apr'
+        break;
+    case 5:
+        monthName = 'May'
+        break;
+    case 6:
+        monthName = 'Jun'
+        break;
+    case 7:
+        monthName = 'Jul'
+        break;
+    case 8:
+        monthName = 'Aug'
+        break;
+    case 9:
+        monthName = 'Sep'
+        break;
+    case 10:
+        monthName = 'Oct'
+        break;
+    case 11:
+        monthName = 'Nov'
+        break;
+    case 12:
+        monthName = 'Dec'
+        break;
+    default:
+        monthName = 'Invalid Month';
+  }
 
   return (
     <div className="absolute top-0 left-0 w-full h-screen flex justify-center items-center flex-col gap-[15px]">
-      <div className="bg-gradient-to-r from-white/60 to-white/20 rounded-[15px] px-[40px] py-[15px] text-[65px] font-spacemono font-[700] tracking-[10px] text-[#22313f]">
-        <span>{date.getHours()}</span>:<span>{date.getMinutes()}</span>:<span>{date.getSeconds()}</span>
+      <p className="font-poppins tracking-[3.5px] text-[#22313f]">
+       <span>{monthName}</span> <span>{date.getDate()}</span>, <span>{date.getFullYear()}</span>
+      </p>
+      <div className="bg-gradient-to-r from-white/60 to-white/20 rounded-[15px] flex items-center tracking-[3.5px] px-[25px] md:px-[35px] py-[15px] text-[40px] md:text-[65px] font-spacemono font-[700] text-[#22313f]">
+        <span>{padZero(date.getHours() % 12)}</span>:
+        <span>{padZero(date.getMinutes())}</span>:
+        <span>{padZero(date.getSeconds())}</span>
+        <span className="font-poppins font-[600] ml-[7px]">{date.getHours() % 12 ? 'PM' : 'AM'}</span>
       </div>
       <div onClick={handlePause}>
         <button className="flex items-center gap-[10px] text-[#22313f] font-poppins py-[10px] px-[20px] rounded-[10px] bg-[#ffffff8c] cursor-pointer">
